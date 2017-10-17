b394d2ffa7df2b0f9a1b10d711e54d6703398b5a
Sidney <sidney.kang@ocsbstudent.ca>
2017-10-17 01:18:27 -0400

    Edited version: added comments and fixed indentation

--- walking_man.py
+++ walking_man.py
@@ -9,49 +9,63 @@ import time
 
 @ui.in_background
 def walking_man_touch_up_inside(sender):
-	
-         number_generated = 0
-         counter = 10
-         while number_generated < counter:
-              if number_generated == 0:
-              	 view['man_walking_image'].image = ui.Image.named('./assets/sprites/walking_man_1.bmp')
-                 number_generated = number_generated + 1                             
-              elif number_generated == 1:
-                 time.sleep(0.15)
-                 view['man_walking_image'].image = ui.Image.named('./assets/sprites/walking_man_2.bmp')                                                                                              
-                 number_generated = number_generated + 1                              
-              elif number_generated == 2:
-                 time.sleep(0.15)
-                 view['man_walking_image'].image = ui.Image.named('./assets/sprites/walking_man_3.bmp')
-                 number_generated = number_generated + 1                             
-              elif number_generated == 3:
-                 time.sleep(0.15)
-                 view['man_walking_image'].image = ui.Image.named('./assets/sprites/walking_man_4.bmp')
-                 number_generated = number_generated + 1                             
-              elif number_generated == 4:
-                 time.sleep(0.15)
-                 view['man_walking_image'].image = ui.Image.named('./assets/sprites/walking_man_5.bmp')
-                 number_generated = number_generated + 1                              
-              elif number_generated == 5:
-                 time.sleep(0.15)
-                 view['man_walking_image'].image = ui.Image.named('./assets/sprites/walking_man_6.bmp')
-                 number_generated = number_generated + 1                            
-              elif number_generated == 6:
-                 time.sleep(0.15)
-                 view['man_walking_image'].image = ui.Image.named('./assets/sprites/walking_man_7.bmp')
-                 number_generated = number_generated + 1                              
-              elif number_generated == 7:
-                 time.sleep(0.15)
-                 view['man_walking_image'].image = ui.Image.named('./assets/sprites/walking_man_8.bmp')
-                 number_generated = number_generated + 1                              
-              elif number_generated == 8:
-                 time.sleep(0.15)
-                 view['man_walking_image'].image = ui.Image.named('./assets/sprites/walking_man_9.bmp')
-                 number_generated = number_generated + 1                             
-              elif number_generated == 9:
-                 time.sleep(0.15)
-                 view['man_walking_image'].image = ui.Image.named('./assets/sprites/walking_man_10.bmp')
-                 number_generated = 0                            
+    # This makes man walk
+   
+    #variables 
+    number_generated = 0
+    counter = 10
+    
+    #process
+    while number_generated < counter:
+          if number_generated == 0:
+             #output
+             view['man_walking_image'].image = ui.Image.named('./assets/sprites/walking_man_1.bmp')
+             number_generated = number_generated + 1                             
+          elif number_generated == 1:
+             time.sleep(0.15)
+             #output
+             view['man_walking_image'].image = ui.Image.named('./assets/sprites/walking_man_2.bmp')                                                                                              
+             number_generated = number_generated + 1                              
+          elif number_generated == 2:
+             time.sleep(0.15)
+             #output
+             view['man_walking_image'].image = ui.Image.named('./assets/sprites/walking_man_3.bmp')
+             number_generated = number_generated + 1                             
+          elif number_generated == 3:
+             time.sleep(0.15)
+             #output
+             view['man_walking_image'].image = ui.Image.named('./assets/sprites/walking_man_4.bmp')
+             number_generated = number_generated + 1                             
+          elif number_generated == 4:
+             time.sleep(0.15)
+             #output
+             view['man_walking_image'].image = ui.Image.named('./assets/sprites/walking_man_5.bmp')
+             number_generated = number_generated + 1                              
+          elif number_generated == 5:
+             time.sleep(0.15)
+             #output
+             view['man_walking_image'].image = ui.Image.named('./assets/sprites/walking_man_6.bmp')
+             number_generated = number_generated + 1                            
+          elif number_generated == 6:
+             time.sleep(0.15)
+             #output
+             view['man_walking_image'].image = ui.Image.named('./assets/sprites/walking_man_7.bmp')
+             number_generated = number_generated + 1                              
+          elif number_generated == 7:
+             time.sleep(0.15)
+             #output
+             view['man_walking_image'].image = ui.Image.named('./assets/sprites/walking_man_8.bmp')
+             number_generated = number_generated + 1                              
+          elif number_generated == 8:
+             time.sleep(0.15)
+             #output
+             view['man_walking_image'].image = ui.Image.named('./assets/sprites/walking_man_9.bmp')
+             number_generated = number_generated + 1                             
+          elif number_generated == 9:
+             time.sleep(0.15)
+             #output
+             view['man_walking_image'].image = ui.Image.named('./assets/sprites/walking_man_10.bmp')
+             number_generated = 0                            
      
 view = ui.load_view()
 view.present('sheet')
